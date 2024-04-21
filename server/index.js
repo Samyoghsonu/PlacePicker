ca39f2de4b06bf8e1522243a76cda9e4f98decb1
SamyoghChilivery <127285267+Samyoghsonu@users.noreply.github.com>
2024-04-22 01:39:13 +0530

    c

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 import fs from "node:fs/promises";
-
 import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors"; // Import cors middleware
 
 const app = express();
 
 app.use(express.static("images"));
 app.use(bodyParser.json());
 
-// CORS
+// Enable CORS for all routes
+app.use(cors());
 
+// Define your routes and other middleware...
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
@@ -17,12 +19,9 @@ app.use((req, res, next) => {
   // console.log("heloo");
   next();
 });
-// app.get("/", (req, res, next) => {
-//   res.send("<h1>Hello Samyogh</h1>");
-//   next();
-// });
 
 app.get("/places", async (req, res) => {
+  // Handle GET /places route
   const fileContent = await fs.readFile("./data/places.json");
 
   const placesData = JSON.parse(fileContent);
@@ -31,6 +30,7 @@ app.get("/places", async (req, res) => {
 });
 
 app.get("/user-places", async (req, res) => {
+  // Handle GET /user-places route
   const fileContent = await fs.readFile("./data/user-places.json");
 
   const places = JSON.parse(fileContent);
@@ -39,6 +39,7 @@ app.get("/user-places", async (req, res) => {
 });
 
 app.put("/user-places", async (req, res) => {
+  // Handle PUT /user-places route
   const places = req.body.places;
 
   await fs.writeFile("./data/user-places.json", JSON.stringify(places));
@@ -46,7 +47,7 @@ app.put("/user-places", async (req, res) => {
   res.status(200).json({ message: "User places updated!" });
 });
 
-// 404
+// Handle 404 error
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
